98734551e9da70a706f14254d7e302cafdfeaa85
ANUJ KUMAR <124524155+anuj9631@users.noreply.github.com>
2024-06-10 00:05:29 +0530

    Update auth.ts

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,25 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-
 import { pusherServer } from "@/app/libs/pusher";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default async function handler(
-  request: NextApiRequest, 
-  response: NextApiResponse
-) {
-  const session = await getServerSession(request, response, authOptions);
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const session = await getServerSession(request, response, authOptions);
 
-  if (!session?.user?.email) {
-    return response.status(401);
+    if (!session?.user?.email) {
+      return response.status(401).json({ error: "Unauthorized" });
+    }
+
+    const { socket_id: socketId, channel_name: channel } = request.body;
+    if (!socketId || !channel) {
+      return response.status(400).json({ error: "Missing socket_id or channel_name in request body" });
+    }
+
+    const data = {
+      user_id: session.user.email,
+    };
+
+    const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
+    return response.status(200).send(authResponse);
+  } catch (error) {
+    console.error("Error authorizing Pusher channel:", error);
+    return response.status(500).json({ error: "Internal Server Error" });
   }
-
-  const socketId = request.body.socket_id;
-  const channel = request.body.channel_name;
-  const data = {
-    user_id: session.user.email,
-  };
-
-  const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
-  return response.send(authResponse);
-};
+}
